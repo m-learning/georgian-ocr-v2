439430d10e8df27c8a86e83e6e7fb0fa9d32f147
Rezo Imnadze <rimnadze@qarva.com>
2017-05-21 18:05:50 +0400

    One script for everything

---
+++ bin/ocr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd `dirname $BASH_SOURCE`/..
+
+rm -f results/words/* 
+rm -f results/meta/*
+
+python src/predict_all.py -i $1
+python src/export.py $1 results/meta exported.svg
+python src/export-fragments.py $1 results/meta results/words exported-fragments.svg
+
