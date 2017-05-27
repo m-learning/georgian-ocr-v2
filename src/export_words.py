407d3697524e50d108d4b7ececdf33586c6e9472
Rezo Imnadze <rimnadze@qarva.com>
2017-05-27 17:06:50 +0400

    Find words

--- src/export_words.py
+++ src/export_words.py
@@ -4,7 +4,7 @@
 import sys
 import os, os.path
 import json
-import cgi
+import codecs
 
 def read_meta(meta_dir):
   all_meta = []
@@ -30,50 +30,74 @@ def get_average_char_width():
 def get_next_line(all_meta, char_meta):
   pass
 
-def get_next_char(all_meta, char_meta, avg_char_width):
+
+
+def get_line_first_chars(all_meta):
+  first = all_meta[83]
+#  for meta in all_meta:
+#    if meta['x'] < first['x']: #and meta['y'] < first['y']:
+#      first = meta
+
+  return [all_meta[83]]
+
+def get_char_lines(all_meta):
+  lines = []
+  for meta in all_meta:
+    pass
+
+def get_all_chars_from_line(all_meta, char_meta, avg_line_height):
   char_x = char_meta['x']
   char_y = char_meta['y']
   char_x_border = char_x + char_meta['w']
-  char_y_border = char_y + char_meta['y']
+  char_y_border = char_y + char_meta['h']
 
+  line_chars = []
   for meta in all_meta:
-    if meta['x'] == char_meta['x'] and meta['y'] == char_meta['y']: continue
+    # Don't add current char
+    #if meta['x'] == char_meta['x'] and meta['y'] == char_meta['y']: continue
 
     meta_x_border = meta['x'] + meta['w']
     meta_y_border = meta['y'] + meta['h']
 
-    # Filter chars from other lines
+    # Filter chars from down lines
     if meta['y'] > char_y_border: continue
 
-    # Filter chars from other words
-    if meta_x_border - char_x_border > avg_char_width / 2: continue
-    
-    # Filter chars before
-    if meta['x'] < char_x: continue
+    # Filter chars from upper lines
+    if meta_y_border < char_y : continue
 
-    return meta
+    line_chars.append(meta)
 
+  sorted_line_chars =  sorted(line_chars, key=lambda ch: ch['x'])
+  return sorted_line_chars
 
-def get_first_char(all_meta):
-  first = all_meta[0]
-  for meta in all_meta:
-    print meta['id'], meta['x'], meta['y'], ' ', first['id'], first['x'], first['y']
-    if meta['x'] < first['x']: #and meta['y'] < first['y']:
-      first = meta
+def split_line_with_words(line_chars, avg_char_width):
+  words = ['']
+  for i in range(0, len(line_chars)-1):
+    char = line_chars[i]
+    next_char = line_chars[i+1]
 
-  return first
+    words[-1]+=char['char']
 
-def get_chars_by_lines(all_meta):
-  pass
+    if next_char['x'] - (char['x']+char['w']) > avg_char_width / 2:
+      # New word is starting here
+      words.append('')
+
+  words[-1]+=next_char['char']
+  return words
+
 
 if __name__ == "__main__":
   all_meta = read_meta('results/meta/')
-  print all_meta
 
-  char = get_first_char(all_meta)
-  while char is not None:
-    print char['char']
-    char = get_next_char(all_meta, char, 40)
-    
+  line_first_chars = get_line_first_chars(all_meta)
 
+  for first_char in line_first_chars:
+    all_line_chars = get_all_chars_from_line(all_meta, line_first_chars[0], 40)
+
+    for ch in all_line_chars:
+      print ch['char'], ch['x'], ch['y']
+
+    words = split_line_with_words(all_line_chars, 24)
+    for w in words:
+      print w
 
