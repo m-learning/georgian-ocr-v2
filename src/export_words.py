d1ce45bd98afa9e688b55bfbca59800b35ed89c6
Rezo Imnadze <rimnadze@qarva.com>
2017-05-27 17:29:42 +0400

    Word exported extracts whole plain document

--- src/export_words.py
+++ src/export_words.py
@@ -13,7 +13,6 @@ def read_meta(meta_dir):
     for f in files:
       fullpath = os.path.join(root, f)
 
-
       # Put new one
       with open(fullpath) as json_data:
         meta_obj = json.load(json_data)
@@ -31,41 +30,32 @@ def get_next_line(all_meta, char_meta):
   pass
 
 
+def is_same_line(char1, char2):
+  char1_y_border = char1['y'] + char1['h']
+  char2_y_border = char2['y'] + char2['h']
 
-def get_line_first_chars(all_meta):
-  first = all_meta[83]
-#  for meta in all_meta:
-#    if meta['x'] < first['x']: #and meta['y'] < first['y']:
-#      first = meta
+  # Char1 is down
+  if char1['y'] > char2_y_border: return False
 
-  return [all_meta[83]]
+  # Char1 is up
+  if char1_y_border < char2['y'] : return False
 
-def get_char_lines(all_meta):
-  lines = []
+  return True
+
+def get_line_first_chars(all_meta, avg_line_height):
+  first_chars = []
   for meta in all_meta:
-    pass
+    sorted_chars = get_all_chars_from_line(all_meta, meta, avg_line_height)
+    if sorted_chars[0] not in first_chars:
+      first_chars.append(sorted_chars[0])
+
+  sorted_first_chars = sorted(first_chars, key=lambda ch: ch['y'])
+  return sorted_first_chars
 
 def get_all_chars_from_line(all_meta, char_meta, avg_line_height):
-  char_x = char_meta['x']
-  char_y = char_meta['y']
-  char_x_border = char_x + char_meta['w']
-  char_y_border = char_y + char_meta['h']
-
   line_chars = []
   for meta in all_meta:
-    # Don't add current char
-    #if meta['x'] == char_meta['x'] and meta['y'] == char_meta['y']: continue
-
-    meta_x_border = meta['x'] + meta['w']
-    meta_y_border = meta['y'] + meta['h']
-
-    # Filter chars from down lines
-    if meta['y'] > char_y_border: continue
-
-    # Filter chars from upper lines
-    if meta_y_border < char_y : continue
-
-    line_chars.append(meta)
+    if is_same_line(meta, char_meta): line_chars.append(meta)
 
   sorted_line_chars =  sorted(line_chars, key=lambda ch: ch['x'])
   return sorted_line_chars
@@ -85,19 +75,22 @@ def split_line_with_words(line_chars, avg_char_width):
   words[-1]+=next_char['char']
   return words
 
-
 if __name__ == "__main__":
   all_meta = read_meta('results/meta/')
 
-  line_first_chars = get_line_first_chars(all_meta)
+  avg_line_height = 40
+  avg_char_width = 24
+  line_first_chars = get_line_first_chars(all_meta, avg_line_height)
 
+  final_text = ''
   for first_char in line_first_chars:
-    all_line_chars = get_all_chars_from_line(all_meta, line_first_chars[0], 40)
+    all_line_chars = get_all_chars_from_line(all_meta, first_char, avg_line_height)
 
-    for ch in all_line_chars:
-      print ch['char'], ch['x'], ch['y']
-
-    words = split_line_with_words(all_line_chars, 24)
+    words = split_line_with_words(all_line_chars, avg_char_width)
     for w in words:
-      print w
+      final_text += w + ' '
+
+    final_text+= '\n'
+
+  print final_text
 
