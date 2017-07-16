6814f58ce031c8f8f0e331249e3782335b932928
Abesalomi Gogatishvili <abesalomi.gogatishvili@gmail.com>
2017-07-16 13:19:21 +0400

    Determine nearest symbols.

--- geo_ocr/fragmenter.py
+++ geo_ocr/fragmenter.py
@@ -10,7 +10,6 @@ import numpy as np
 import traceback
 from scipy import ndimage
 
-
 from skimage import color
 from skimage import filters
 from skimage import img_as_ubyte
@@ -25,190 +24,191 @@ DEBUG_DIR = "results/debug"
 
 
 def create_clean_dir(path):
-    if os.path.exists(path):
-        shutil.rmtree(path, ignore_errors=True)
+	if os.path.exists(path):
+		shutil.rmtree(path, ignore_errors=True)
 
-    os.makedirs(path)
+	os.makedirs(path)
 
 
-def vanish_image(img,invert=False):
-    gray_scale_image = color.rgb2gray(img)
-    if invert:
-        gray_scale_image = util.invert(gray_scale_image)
-    val = filters.threshold_local(gray_scale_image,101,mode="wrap",offset=0.1)
-    return (gray_scale_image > val)
+def vanish_image(img, invert=False):
+	gray_scale_image = color.rgb2gray(img)
+	if invert:
+		gray_scale_image = util.invert(gray_scale_image)
+	val = filters.threshold_local(gray_scale_image, 101, mode="wrap", offset=0.1)
+	return (gray_scale_image > val)
 
-       
+
 def find_noise(data):
-    width = data["meta"]["w"]
-    if width < 13:
-        return True
-    return False
+	width = data["meta"]["w"]
+	if width < 8:
+		return True
+	return False
 
 
-def delete_subcrops(allMeta, img_arrays, debug = True):
+def delete_subcrops(allMeta, img_arrays, debug=True):
+	for m1 in allMeta:
+		for m2 in allMeta:
+			if (m1['x'] > m2['x'] and
+					    m1['y'] > m2['y'] and
+						    m1['x'] + m1['w'] < m2['x'] + m2['w'] and
+						    m1['y'] + m1['h'] < m2['y'] + m2['h']):
 
-    for m1 in allMeta:
-       for m2 in allMeta:
-           if (m1['x'] > m2['x'] and
-                  m1['y'] > m2['y'] and
-                  m1['x']+m1['w'] < m2['x']+m2['w'] and
-                  m1['y']+m1['h'] < m2['y']+m2['h']):
+				img_arrays = [s for s in img_arrays if not s['meta']['id'] == m1['id']]
 
-              img_arrays = [s for s in img_arrays if not s['meta']['id'] == m1['id']]
+				if debug:
+					imageFilename = "%s/%d.png" % (FRAGMENTS_DIR, m1['id'])
+					os.remove(imageFilename)
 
-              if debug:
-                imageFilename = "%s/%d.png" % (FRAGMENTS_DIR, m1['id'])
-                os.remove(imageFilename)
-
-    return img_arrays
+	return img_arrays
 
 
-def do_fragmentation(file_path, debug = True):
-    create_clean_dir(FRAGMENTS_DIR)
-    create_clean_dir(META_DIR)
-    create_clean_dir(DEBUG_DIR)
-    
-    # load source image
-    src_img = cv2.imread(file_path)
+def do_fragmentation(file_path, debug=True):
+	create_clean_dir(FRAGMENTS_DIR)
+	create_clean_dir(META_DIR)
+	create_clean_dir(DEBUG_DIR)
 
-    gray = vanish_image(src_img)
-    #gray=util.invert(gray)
-    # convert to grayscale
-    # gray = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(("%s/a1 gray.png" % DEBUG_DIR), gray * 255)
+	# load source image
+	src_img = cv2.imread(file_path)
 
-    # smooth the image to avoid noises
-    # gray = cv2.medianBlur(gray, 5)
-    # cv2.imwrite(("%s/a2 medianBlur.png" % DEBUG_DIR), gray)
-    
-    # Apply adaptive threshold
-    # thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
-    # thresh_color = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+	gray = vanish_image(src_img)
+	# gray=util.invert(gray)
+	# convert to grayscale
+	# gray = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
+	cv2.imwrite(("%s/a1 gray.png" % DEBUG_DIR), gray * 255)
 
-    # cv2.imwrite(("%s/a3 treshColor.png" % DEBUG_DIR), thresh_color)
-    
-    # apply some dilation and erosion to join the gaps
-    # thresh = cv2.dilate(thresh, None, iterations=3)
-    # cv2.imwrite(("%s/a4 deliate.png" % DEBUG_DIR), thresh)
-    
-    # thresh = cv2.erode(thresh, None, iterations=2)
-    # cv2.imwrite(("%s/a5 erode.png" % DEBUG_DIR), thresh)
-    
-    # Find the contours
-    cv_image = img_as_ubyte(gray)
-    _, contours, hierarchy = cv2.findContours(cv_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    
-    # TODO: allMeta is not required because img_arrays contains meta any way
-    allMeta = []
-    count = 0
+	# smooth the image to avoid noises
+	# gray = cv2.medianBlur(gray, 5)
+	# cv2.imwrite(("%s/a2 medianBlur.png" % DEBUG_DIR), gray)
 
-    img_arrays = []
+	# Apply adaptive threshold
+	# thresh = cv2.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
+	# thresh_color = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
 
-    print 'Number of contures', len(contours)
-    # For each contour, find the bounding rectangle and crop it.
-    # put cropped image on a blank background and write to disk
-    for cnt in contours:
-        try:
-            # Create image file
-            x, y, w, h, img_arr = crop_rectangle(cv_image, cnt)
+	# cv2.imwrite(("%s/a3 treshColor.png" % DEBUG_DIR), thresh_color)
 
-            if debug:
-                cv2.imwrite(("%s/%s.png" % (FRAGMENTS_DIR, count)), img_arr)
+	# apply some dilation and erosion to join the gaps
+	# thresh = cv2.dilate(thresh, None, iterations=3)
+	# cv2.imwrite(("%s/a4 deliate.png" % DEBUG_DIR), thresh)
 
-            # Create meta file
-            meta = {'x': x, 'y': y, 'w': w, 'h': h, 'id': count}
-            
-            image_data = {'arr':img_arr, "meta": meta}
-            if not find_noise(image_data):
-                img_arrays.append(image_data)
-                allMeta.append(meta)
-                count += 1
-        except ValueError, ve:
-            if debug:
-                traceback.print_exc(file=sys.stdout)
-                print "skip fragment:", ve
+	# thresh = cv2.erode(thresh, None, iterations=2)
+	# cv2.imwrite(("%s/a5 erode.png" % DEBUG_DIR), thresh)
 
-    # TODO: Last contour is the whole image. It has to be deleted nicely
-    img_arrays = img_arrays[:-1]
-    allMeta = allMeta[:-1]
-    img_arrays = delete_subcrops(allMeta, img_arrays, debug)
-    return img_arrays
-    
-    
+	# Find the contours
+	cv_image = img_as_ubyte(gray)
+	_, contours, hierarchy = cv2.findContours(cv_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+	# TODO: allMeta is not required because img_arrays contains meta any way
+	allMeta = []
+	count = 0
+
+	img_arrays = []
+
+	print 'Number of contures', len(contours)
+	# For each contour, find the bounding rectangle and crop it.
+	# put cropped image on a blank background and write to disk
+	for cnt in contours:
+		try:
+			# Create image file
+			x, y, w, h, img_arr = crop_rectangle_by_contour(cv_image, cnt)
+
+			if debug:
+				cv2.imwrite(("%s/%s.png" % (FRAGMENTS_DIR, count)), img_arr)
+
+			# Create meta file
+			meta = {'x': x, 'y': y, 'w': w, 'h': h, 'id': count}
+
+			image_data = {'arr': img_arr, "meta": meta}
+			if not find_noise(image_data):
+				img_arrays.append(image_data)
+				allMeta.append(meta)
+				count += 1
+		except ValueError, ve:
+			if debug:
+				traceback.print_exc(file=sys.stdout)
+				print "skip fragment:", ve
+
+	# TODO: Last contour is the whole image. It has to be deleted nicely
+	img_arrays = img_arrays[:-1]
+	allMeta = allMeta[:-1]
+	img_arrays = delete_subcrops(allMeta, img_arrays, debug)
+	return (img_arrays, cv_image)
+
+
 def create_blank_image(width=64, height=64, rgb_color=(255, 255, 255)):
-    image = np.zeros((height, width, 3), np.uint8)
-    
-    # Since OpenCV uses BGR, convert the color first
-    color = tuple(reversed(rgb_color))
-    # Fill image with color
-    image[:] = color
-    
-    return image
+	image = np.zeros((height, width, 3), np.uint8)
+
+	# Since OpenCV uses BGR, convert the color first
+	color = tuple(reversed(rgb_color))
+	# Fill image with color
+	image[:] = color
+
+	return image
 
 
 def create_image_for_recognize(image, width=64, height=64):
-    generated_image = np.ones((height, width)) * 255
-    (image_h, image_w) = image.shape
-    index_w = (width - image_w) / 2
-    index_h = (height - image_h) / 2
+	generated_image = np.ones((height, width)) * 255
+	(image_h, image_w) = image.shape
+	index_w = (width - image_w) / 2
+	index_h = (height - image_h) / 2
 
-    generated_image[index_h : image_h + index_h, index_w : image_w + index_w] = image
-    return generated_image
+	generated_image[index_h: image_h + index_h, index_w: image_w + index_w] = image
+	return generated_image
 
 
 def downscale_proportionally(image, max_w, max_h):
-    h, w = image.shape[:2]
+	h, w = image.shape[:2]
 
-    if w > h:
-      target_w = max_w
-      # TODO: Remove magic numbers (1.5)
-      target_h = int(max_h * (float(max_w) / w) * 1.5) 
-    else:
-      target_w = int(max_w * (float(max_h) / h) * 1.5)
-      target_h = max_h
+	if w > h:
+		target_w = max_w
+		# TODO: Remove magic numbers (1.5)
+		target_h = int(max_h * (float(max_w) / w) * 1.5)
+	else:
+		target_w = int(max_w * (float(max_h) / h) * 1.5)
+		target_h = max_h
 
-    # FIXME: This must not happen
-    if target_w == 0 or target_h == 0:
-      return image
+	# FIXME: This must not happen
+	if target_w == 0 or target_h == 0:
+		return image
 
-    crop_img = cv2.resize(image, (target_w, target_h))
-    return crop_img
+	crop_img = cv2.resize(image, (target_w, target_h))
+	return crop_img
 
 
-def crop_rectangle(img, contour):
-    x, y, w, h = cv2.boundingRect(contour)
-    
-    crop_img = img[y:y + h, x:x + w]
-    
-    # define small height and width
-    s_height, s_width = crop_img.shape[:2]
+def crop_rectangle_by_contour(img, contour):
+	x, y, w, h = cv2.boundingRect(contour)
 
-    # Shrink if cropped image is oversized
-    if s_height > 64 or s_width > 64:
-      crop_img = downscale_proportionally(crop_img, 40, 40)
+	y2, x2 = y + h, x + w
 
-    # define background image as large image 
-    result_img = create_blank_image()
+	crop_img = img[y:y2, x:x2]
 
-    # define large height and width
-    l_height, l_width = result_img.shape[:2]
-    
-    y_offset = int(math.floor((l_height - s_height) / 2))
-    x_offset = int(math.floor((l_width - s_width) / 2))
-    	
-    ndimage.gaussian_filter(crop_img, 1, output=crop_img)
-    
-    # Convert image to 64x64
-    image_to_recognize = create_image_for_recognize(crop_img)
-    
-    return x, y, w, h, image_to_recognize
+	# define small height and width
+	s_height, s_width = crop_img.shape[:2]
+
+	# Shrink if cropped image is oversized
+	if s_height > 64 or s_width > 64:
+		crop_img = downscale_proportionally(crop_img, 40, 40)
+
+	# define background image as large image
+	result_img = create_blank_image()
+
+	# define large height and width
+	l_height, l_width = result_img.shape[:2]
+
+	y_offset = int(math.floor((l_height - s_height) / 2))
+	x_offset = int(math.floor((l_width - s_width) / 2))
+
+	ndimage.gaussian_filter(crop_img, 1, output=crop_img)
+
+	# Convert image to 64x64
+	image_to_recognize = create_image_for_recognize(crop_img)
+
+	return x, y, w, h, image_to_recognize
 
 
 if __name__ == "__main__":
-    # source file path
-    if len(sys.argv) > 1:
-        source_file_path = sys.argv[1]
-        do_fragmentation(source_file_path)
-    else:
-        print ("Invalid argument: <source file>")
+	# source file path
+	if len(sys.argv) > 1:
+		source_file_path = sys.argv[1]
+		do_fragmentation(source_file_path)
+	else:
+		print ("Invalid argument: <source file>")
--- geo_ocr/read.py
+++ geo_ocr/read.py
@@ -3,33 +3,61 @@ from predict_all import *
 
 
 def read(image_path, debug=True):
-    if not os.path.isfile(image_path):
-        print("Files does not exists")
-        return
+	if not os.path.isfile(image_path):
+		print("Files does not exists")
+		return
 
-    image_arrays = fragmenter.do_fragmentation(image_path, debug=debug)
-    full_score = 0
-    full_count = 0
+	image_arrays = fragmenter.do_fragmentation(image_path, debug=debug)
+	full_score = 0
+	full_count = 0
 
-    for n in image_arrays:
-        img_arr = n["arr"]
-        meta_data = n["meta"]
-        img_arr = img_arr.flatten()
-        [char, score] = recognize_image(img_arr)
-        full_score += score
-        full_count += 1
-        if debug:
-            print meta_data, char.encode('utf8'), score
-        meta_data['char'] = char
-        meta_data['score'] = score
-    if debug:
-        print 'Avg score: %d' % (full_score * 100 / full_count)
+	for n in image_arrays:
+		img_arr = n["arr"]
+		meta_data = n["meta"]
+		img_arr = img_arr.flatten()
+		print meta_data['id'], get_nearest(n, image_arrays)
+		continue
+		[char, score] = recognize_image(img_arr)
+		full_score += score
+		full_count += 1
+		if debug:
+			print meta_data, char.encode('utf8'), score
+		meta_data['char'] = char
+		meta_data['score'] = score
+	if debug:
+		print 'Avg score: %d' % (full_score * 100 / full_count)
 
-    # ----- for testing --------
-    # use export_word as a module
-    export_words.test([n["meta"] for n in image_arrays])
+	# ----- for testing --------
+	# use export_word as a module
+	export_words.test([n["meta"] for n in image_arrays])
+
+
+def get_nearest(image, image_arrays, n=4):
+	distances = []
+	for im in image_arrays:
+		if im is image:
+			continue
+		distances.append((calculate_distance(image, im), im['meta']))
+	sorted_distances = sorted(distances, key=lambda dist: dist[0])[:n]
+
+	return [get_frame(image['meta'], im) for (d, im) in sorted_distances]
+
+
+def get_frame(meta1, meta2):
+	top = min(meta1['y'], meta2['y'])
+	right = min(meta1['x'] + meta1['w'], meta2['x'] + meta2['w'])
+	bottom = min(meta1['y'] + meta1['h'], meta2['y'] + meta2['h'])
+	left = min(meta1['x'], meta2['x'])
+	return [top, right, bottom, left]
+
+
+def calculate_distance(im1, im2):
+	meta1, meta2 = im1["meta"], im2["meta"]
+	x1, y1, = meta1['x'] + meta1['w'] / 2, meta1['y'] + meta1['h'] / 2
+	x2, y2 = meta2['x'] + meta2['w'] / 2, meta2['y'] + meta2['h'] / 2
+	return pow(x1 - x2, 2) + pow(y1 - y2, 2)
 
 
 if __name__ == '__main__':
-    args = init_arguments()
-    read(args.image, args.debug)
+	args = init_arguments()
+	read(args.image, args.debug)
