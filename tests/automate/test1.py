66098ce14f8c91188b8f55887622e5924ce178d2
nutu <ninouturashvili@gmail.com>
2017-07-30 15:07:18 +0400

    decode utf-8

--- tests/automate/test1.py
+++ tests/automate/test1.py
@@ -3,21 +3,21 @@ import geo_ocr
 import subprocess
 from Levenshtein import ratio
 
-a=["160.txt", "220.txt", "cxoveli.txt", "logikuri.txt", "logikuri-mcdaria.txt", "mqsoveli.txt", "mtacebeli.txt", "nika.txt", "procenti.txt", "proporciuli.txt" , "datom.txt" , "gafrinda.txt" , "miigos.txt"]
+textfiles=["160.txt",  "cxoveli.txt", "logikuri.txt", "logikuri-mcdaria.txt", "mqsoveli.txt", "mtacebeli.txt", "nika.txt", "procenti.txt", "proporciuli.txt" , "datom.txt" , "gafrinda.txt" , "miigos.txt" , "220.txt"]
 
-b=["160.jpg","220.jpg","cxoveli.jpg","logikuri.jpg","logikuri-mcdaria.jpg","mqsoveli.jpg","mtacebeli.jpg","nika.jpg","procenti.jpg","proporciuli.jpg" , "datom.jpg" , "gafrinda.jpg" , "miigos.jpg"]
+b=["160.jpg","cxoveli.jpg","logikuri.jpg","logikuri-mcdaria.jpg","mqsoveli.jpg","mtacebeli.jpg","nika.jpg","procenti.jpg","proporciuli.jpg" , "datom.jpg" , "gafrinda.jpg" , "miigos.jpg" ,"220.jpg"]
 
 i=0
 y=0
 total =0
 testresults = []
-while i < (len(a)):
-	txt = open("tests/automate/level1/"+a[i], "r+")
+while i < (len(textfiles)):
+	txt = open("tests/automate/level1/"+textfiles[i], "r+")
 	data = txt.read()
-	print a[i] , b[i]
+	print textfiles[i] , b[i]
 	try:
 		pic =open("tests/automate/level1/output.txt" , "wr")
-		pic.write(geo_ocr.read("tests/automate/level1/"+b[i], True, False).encode('utf-8').strip())
+		pic.write(geo_ocr.read("tests/automate/level1/"+b[i], False, False).decode('utf-8').encode('utf-8'))
 		pic = open("tests/automate/level1/output.txt" , "r")
 		result = pic.read()
 		testresults.append(ratio(data , result))
@@ -28,12 +28,12 @@ while i < (len(a)):
 
 		total += ratio(data , result)
 	except Exception as e:
-		print "error ocured with ", a[i] , b[i], e
+		print "error ocured with ", textfiles[i] , b[i], e
 		testresults.append(0)
 		total +=0
 	finally:
 		i+=1
-average = total/len(a)
+average = total/len(textfiles)
 #green = ( '\033[92m'%round(testresults[y] *100, 1) + '\033[0m')
 #red = ( '\033[91m' %round(testresults[y] *100, 1) + '\033[0m')
 for y in range(len(testresults)):
@@ -43,6 +43,6 @@ for y in range(len(testresults)):
 		print ( '\033[91m'+ b[y]+ '\033[0m') , round(testresults[y] *100, 1), "%"
 	y+=1
 if average > 0.7:
-	print  ( '\033[92m' + "LEVEL 1 AVERAGE "+ '\033[0m') , round(total/len(a) *100 , 1) , "%"
+	print  ( '\033[92m' + "LEVEL 1 AVERAGE "+ '\033[0m') , round(total/len(textfiles) *100 , 1) , "%"
 else:
-	print ( '\033[91m' +  "LEVEL 1 AVERAGE " + '\033[0m') , round(total/len(a) *100 , 1) , "%"
+	print ( '\033[91m' +  "LEVEL 1 AVERAGE " + '\033[0m') , round(total/len(textfiles) *100 , 1) , "%"
--- tests/automate/test2.py
+++ tests/automate/test2.py
@@ -3,28 +3,34 @@ import geo_ocr
 import subprocess
 from Levenshtein import ratio
 
-a=["bad-light.txt", "charkviani.txt", "mnatobi.txt", "mnatobi-koshmari.txt", "mnatobi-lines.txt", "mnatobi-mini.txt", "mtavruli.txt", "mtavruli-bold.txt", "phone-1.txt"]
+textfiles=["bad-light.txt", "charkviani.txt", "mnatobi.txt", "mnatobi-koshmari.txt", "mnatobi-lines.txt", "mnatobi-mini.txt", "mtavruli.txt", "mtavruli-bold.txt", "phone-1.txt"]
 b=["bad-light.jpg", "charkviani.jpg", "mnatobi.png", "mnatobi-koshmari.png", "mnatobi-lines.png", "mnatobi-mini.png", "mtavruli.png", "mtavruli-bold.png", "phone-1.jpg"]
 i=0
 y=0
 total =0
 testresults = []
-while i < (len(a)):
-	txt = open("tests/automate/level2-badlight/"+a[i], "r+")
+while i < (len(textfiles)):
+	txt = open("tests/automate/level2-badlight/"+textfiles[i], "r+")
 	data = txt.read()
-	print a[i] , b[i]
-	pic =open("tests/automate/level2-badlight/output.txt" , "wr")
-	pic.write(geo_ocr.read("tests/automate/level2-badlight/"+b[i], True, False).encode('utf-8').strip())
-	pic = open("tests/automate/level2-badlight/output.txt" , "r")
-	result = pic.read()
-	testresults.append(ratio(data , result))
+	print textfiles[i] , b[i]
+	try:
+		pic =open("tests/automate/level2-badlight/output.txt" , "wr")
+		pic.write(geo_ocr.read("tests/automate/level2-badlight/"+b[i], False, False).decode('utf-8').encode('utf-8'))
+		pic = open("tests/automate/level2-badlight/output.txt" , "r")
+		result = pic.read()
+		testresults.append(ratio(data , result))
 
-	print "data" , data
-	print  "result" , result
-	print  b[i], ratio(data , result)
-	total += ratio(data , result)
-	i+=1
-average = total/len(a)
+		print "data" , data
+		print  "result" , result
+		print  b[i], ratio(data , result)
+		total += ratio(data , result)
+	except Exception as e:
+		print "error ocured with ", textfiles[i] , b[i], e
+		testresults.append(0)
+		total +=0
+	finally:	
+		i+=1
+average = total/len(textfiles)
 #green = ( '\033[92m'%round(testresults[y] *100, 1) + '\033[0m')
 #red = ( '\033[91m' %round(testresults[y] *100, 1) + '\033[0m')
 for y in range(len(testresults)):
@@ -34,6 +40,6 @@ for y in range(len(testresults)):
 		print ( '\033[91m'+ b[y]+ '\033[0m') , round(testresults[y] *100, 1), "%"
 	y+=1
 if average > 0.7:
-	print  ( '\033[92m' +"LEVEL 2 AVERAGE " + '\033[0m') , round(total/len(a) *100 , 1) , "%"
+	print  ( '\033[92m' +"LEVEL 2 AVERAGE " + '\033[0m') , round(total/len(textfiles) *100 , 1) , "%"
 else:
-	print ( '\033[91m' +  "LEVEL 2 AVERAGE "+ '\033[0m') , round(total/len(a) *100 , 1) , "%"
+	print ( '\033[91m' +  "LEVEL 2 AVERAGE "+ '\033[0m') , round(total/len(textfiles) *100 , 1) , "%"
