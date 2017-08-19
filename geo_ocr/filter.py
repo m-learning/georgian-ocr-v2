2abc59a16b442699a815186ad8315df397d72549
Shota Amashukeli <shamashukeli@gmail.com>
2017-08-19 20:29:41 +0400

    added out of line filter

--- geo_ocr/filter.py
+++ geo_ocr/filter.py
@@ -253,34 +253,37 @@ def filter_out_of_line(lines):
     changed=True
     print "---------------------------sadasd----------" 
     #print lines[0][0]['char']
-    
-    for line in lines:
-        leng=len(line)         
-        for i in range(leng):
-            if line[leng-1-i]['char'] == ' ':
-                break
-        #if line[leng-i]['char']=='*':
-        if leng==1:
-            continue
-        if i>0:
-            if any(line[leng-i]['char']==char for char in faulty):
-                print line[leng-i]['char']
-                line=line[:leng-i]
-        new_lines.append(line)
-    lines=new_lines[:]
-    new_lines=[]
-    
-    for line in lines:
-        leng=len(line)         
-        for i in range(leng):
-            if line[i]['char'] == ' ':
-                print i
-                break
-        if i+1>0:
-            if any(line[i-1]['char']==char for char in faulty):
-                print line[i-1]['char']
-                line=line[i+1:]
-        new_lines.append(line)
+    while(cahnged): 
+        changed=False
+        for line in lines:
+            leng=len(line)         
+            for i in range(leng):
+                if line[leng-1-i]['char'] == ' ':
+                    break
+            #if line[leng-i]['char']=='*':
+            if leng==1:
+                continue
+            if i>0:
+                if any(line[leng-i]['char']==char for char in faulty):
+                    print line[leng-i]['char']
+                    line=line[:leng-i]
+                    changed=True
+            new_lines.append(line)
+        lines=new_lines[:]
+        new_lines=[]
+        
+        for line in lines:
+            leng=len(line)         
+            for i in range(leng):
+                if line[i]['char'] == ' ':
+                    print i
+                    break
+            if i+1>0:
+                if any(line[i-1]['char']==char for char in faulty):
+                    print line[i-1]['char']
+                    line=line[i+1:]
+                    changed=True
+            new_lines.append(line)
     for line in new_lines:
         for char in line:
             ssst+=char['char']
