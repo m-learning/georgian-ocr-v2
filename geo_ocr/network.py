bd3d13a7d6fca0fb5b68d98208444e3b3f7c090b
sosoxuc <soso_xuc@yahoo.com>
2017-08-25 21:58:23 +0400

    remove bad symbols

--- geo_ocr/network.py
+++ geo_ocr/network.py
@@ -25,14 +25,6 @@ def init_model(nb_classes, input_shape):
 	model.add(Activation('relu'))
 	model.add(Dropout(0.2))
 
-	model.add(Dense(1048))
-	model.add(Activation('relu'))
-	model.add(Dropout(0.2))
-
-	model.add(Dense(512))
-	model.add(Activation('relu'))
-	model.add(Dropout(0.2))
-
 	model.add(Dense(nb_classes))
 	model.add(Activation('softmax'))
 	return model
