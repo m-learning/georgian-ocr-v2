81363210e7da64442fdf3a4208f6ba0abf75bd76
sosoxuc <soso_xuc@yahoo.com>
2017-08-26 09:58:32 +0400

    remove bad symbols

--- geo_ocr/network.py
+++ geo_ocr/network.py
@@ -3,13 +3,13 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Dense, Dropout, Activation, Flatten
 
 nb_filters = 16
-pool_size = (2, 2)
+pool_size = (3, 3)
 kernel_size = (3, 3)
 def init_model(nb_classes, input_shape):
 
 	model = Sequential()
 	
-	model.add(Convolution2D(nb_filters, kernel_size, padding='valid', input_shape=input_shape, use_bias=True, strides = 2))
+	model.add(Convolution2D(nb_filters, kernel_size, padding='valid', input_shape=input_shape, use_bias=True))
 	model.add(Activation('relu'))
 	model.add(MaxPooling2D(pool_size=pool_size))
 
