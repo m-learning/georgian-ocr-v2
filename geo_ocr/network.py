36e0e673eec46e1ed511881ac11ea50f76aa23dd
sosoxuc <soso_xuc@yahoo.com>
2017-08-26 10:53:51 +0400

    remove bad symbols

--- geo_ocr/network.py
+++ geo_ocr/network.py
@@ -11,19 +11,22 @@ def init_model(nb_classes, input_shape):
 	
 	model.add(Convolution2D(8, kernel_size, padding='valid', input_shape=input_shape, use_bias=True))
 	model.add(Activation('relu'))
-	#model.add(MaxPooling2D(pool_size=pool_size))
+	model.add(MaxPooling2D(pool_size=pool_size))
+	model.add(Dropout(0.1))
 
 	model.add(Convolution2D(32, kernel_size, use_bias=True, padding='valid'))
 	model.add(Activation('relu'))
 	model.add(MaxPooling2D(pool_size=pool_size))
-	model.add(Dropout(0.2))
+	model.add(Dropout(0.1))
 
 	model.add(Flatten())
 
 	model.add(Dense(256))
 	model.add(Activation('relu'))
+  model.add(Dropout(0.1))
 
 	model.add(Dense(nb_classes))
 	model.add(Activation('softmax'))
 	model.summary()
 	return model
+
