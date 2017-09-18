9ed14889d34f8d0b0b15393c13b77dbf610ae545
giorgi <giorgishvelidze@gmail.com>
2017-09-18 20:44:30 +0400

    training-data-collector - added flask session

--- collector/app.py
+++ collector/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, session, request, render_template, jsonify, escape
 from random import randint
 import os
 import base64
@@ -12,6 +12,8 @@ sys.path.insert(0, '../geo_ocr/')
 import read
 
 app = Flask(__name__)
+
+app.secret_key = '31D041CEB9916A0AF926B496445BA6B2'
 
 def createDirIfNotExists(directory):
     if not os.path.isdir(directory):
@@ -32,29 +34,35 @@ createDirIfNotExists(UPLOADED_IMAGES_DIR)
 createDirIfNotExists(RESULT_IMAGES_DIR)
 createDirIfNotExists(TRAINING_DATA_DIR)
 
-last_image_path = 0
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global last_image_path
     if request.method == 'GET':
-        if last_image_path != 0:
-            return render_template('index.html')
+        if 'last_image_path' in session:
+            if session['last_image_path'] != 0:
+                return render_template('index.html')
+            else:
+                return render_template('upload.html')
         else:
             return render_template('upload.html')
+
     elif request.method == 'POST' and 'image' in request.files:
         filename = str(len(os.walk(UPLOADED_IMAGES_DIR).next()[2]) + 1)
         last_image_path = os.path.join(UPLOADED_IMAGES_DIR, filename)
         _file = request.files['image']
         _file.save(last_image_path)
         print "File " + filename + " was uploaded successfully"
+        session['last_image_path'] = last_image_path
         return render_template('index.html')
 
 @app.route('/load', methods=['GET'])
 def load():
     # TODO run read.py for last_image_path
     # return json array of base64 strings of extracted images
-    global last_image_path
+    if 'last_image_path' in session:
+        last_image_path = session['last_image_path']
+    else:
+        last_image_path = ''
+
     print last_image_path
 
     if not os.path.isfile(last_image_path):
@@ -63,11 +71,10 @@ def load():
 
     filenames = []
 
-    result = read.read_lines(last_image_path)    
-    print result
-    return ""
+    #char_images = read.read_lines(last_image_path)    
+    #print char_images
+    return jsonify([])
     
-    #char_images = read.read_lines(last_image_path)
     for i in range(len(char_images)):
         filename = os.path.join(RESULT_IMAGES_DIR, str(i) + ".png")
         filenames.append(filename)
@@ -88,8 +95,7 @@ def read_image(src):
 @app.route('/clear', methods=['GET'])
 def clear():
     print "clear"
-    global last_image_path
-    last_image_path = 0
+    session['last_image_path'] = 0
     return ""
 
 @app.route("/save", methods=['POST'])
