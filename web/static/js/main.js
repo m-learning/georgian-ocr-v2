5aed13670c3fbf2710d56a37dc8c7d23b150e537
Davit Kobaladze <davitkobaladze3@gmail.com>
2018-03-21 18:58:05 +0400

    added spinner.

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-  $("#get_file").on('click', function(){
-    $("#my_file").trigger('click');
-  });
-  $("#my_file").change(function(){
-    $("#read").submit();
-  });
+$(document).ready(function() {
+    $("#get_file").on('click', function() {
+        $("#my_file").trigger('click');
+    });
+    $("#my_file").change(function() {
+        $("#read").submit();
+        $("#read").append('<div class="loader"></div>');
+        $("#read").append('<p class="loader-text" style="color:red;font-size:small;">მიმდინარეობს წაკითხვა და pdf ფაილად გარდაქმნა</p>');
+    });
+    $(window).blur(function() {
+        if ($('.loader').length) {
+            $('.loader').remove();
+            $('.loader-text').remove();
+        }
+    })
 });
